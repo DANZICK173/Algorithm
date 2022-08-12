0a0845162cb2fd1ee714b43e78004ca48748850f
DanGheorghita <upikl@student.kit.edu>
2022-08-12 19:27:13 +0200

    QuickSort added

---
+++ Sorting/QuickSort/QuickSort.java
@@ -0,0 +1,45 @@
+public class QuickSort {
+
+    public static void main(String[] args) {
+        int[] array = { 2, 45, 55, 23, 13, 20 };
+        quickSort(array, 0, array.length - 1);
+        printArray(array);
+    }
+
+    public static void quickSort(int[] array, int begin, int end) {
+        if (begin < end) {
+            int partitionIndex = partition(array, begin, end);
+
+            quickSort(array, begin, partitionIndex - 1);
+            quickSort(array, partitionIndex + 1, end);
+        }
+    }
+
+    public static int partition(int[] array, int begin, int end) {
+        int pivot = array[end];
+        int i = (begin - 1);
+
+        for (int j = begin; j < end; j++) {
+            if (array[j] <= pivot) {
+                i++;
+
+                int swapTemp = array[i];
+                array[i] = array[j];
+                array[j] = swapTemp;
+            }
+        }
+
+        int swapTemp = array[i + 1];
+        array[i + 1] = array[end];
+        array[end] = swapTemp;
+
+        return i + 1;
+    }
+
+    public static void printArray(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i] + " ");
+        }
+    }
+
+}
